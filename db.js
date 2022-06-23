b654823f25547f789cfdae9117fe90e4921a2df6
riandroraul <riandroraul@gmail.com>
2022-06-23 17:03:36 +0700

    first commit

--- db.js
+++ db.js
@@ -1,27 +1,8 @@
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+var {MongoClient} = require('mongodb');
+var url = "mongodb://localhost:27017/mydb";
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'myProject';
-
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
-
-  // the following code examples can be pasted here...
-
-  return 'done.';
-}
-
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally( () => client.close())
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
