b4bce160bbc4cbd8e573e39190698c3815386375
riandroraul <riandroraul@gmail.com>
2022-06-24 16:58:34 +0700

    coba core modules

--- cobaCoreModules.js
+++ cobaCoreModules.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs');
+const { resolve } = require('path');
 const { stdout } = require('process')
 
 // menuliskan string kedalam file secara synchrounous
@@ -31,7 +32,72 @@ const rl = readline.createInterface({
     output: stdout,
 });
 
-rl.question('masukkan nama anda: ', (nama) => {
-    console.log(`Terima kasih ${nama}`);
+// membuat folder baru jika belum ada
+const dirPath = './data';
+if(!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath)
+}
+
+// membuat file baru jika belum ada
+const filePath = './data/books.json';
+if(!fs.existsSync(filePath)){
+    fs.writeFileSync(filePath, '[]', 'utf-8')
+}
+
+const pertanyaan1  = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('masukkan nama buku: ', (namaBuku) => {
+            resolve(namaBuku)
+        })
+    })
+}
+
+const pertanyaan2  = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('masukkan nama penerbit: ', (penerbit) => {
+            resolve(penerbit)
+        })
+    })
+}
+
+const pertanyaan3 = () => {
+    return new Promise((resolve, reject) => {
+        rl.question('masukkan nama pengarang: ', (pengarang) => {
+            resolve(pengarang)
+        });
+    });
+}
+
+const main = async () => {
+    const namaBuku = await pertanyaan1()
+    const penerbit = await pertanyaan2()
+    const pengarang = await pertanyaan3()
+
+    const book = {namaBuku, penerbit, pengarang}
+
+    const file = fs.readFileSync('data/books.json', 'utf-8')
+    const books = JSON.parse(file)
+    
+    books.push(book)
+    fs.writeFileSync('data/books.json', JSON.stringify(books))
+    console.log('berhasil')
     rl.close();
-})
+}
+
+main()
+
+// rl.question('masukkan nama buku: ', (namaBuku) => {
+//     rl.question('masukkan nama penerbit: ', (penerbit) => {
+//         rl.question('masukkan nama pengarang: ', (pengarang) => {
+
+//             const book = {namaBuku, penerbit, pengarang}
+//             const file = fs.readFileSync('data/books.json', 'utf-8')
+//             const books = JSON.parse(file)
+            
+//             books.push(book)
+//             fs.writeFileSync('data/books.json', JSON.stringify(books))
+//             console.log('berhasil')
+//             rl.close();
+//         })
+//     })
+// })
